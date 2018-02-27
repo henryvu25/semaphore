998c160af92d9473616b70fe53ef3c00e4a197d4
Henry Vu <henryvu25@gmail.com>
2018-02-26 18:36:52 -0800

    finished semaphore section

--- Controllers/SemController.cs
+++ Controllers/SemController.cs
@@ -21,7 +21,103 @@ namespace semaphore_proj.Controllers
         [Route("dashboard")]
         public IActionResult Dashboard()
         {
+            int? currId = HttpContext.Session.GetInt32("currId");
+            if(currId == null)
+            {
+                return RedirectToAction("Logout", "Home");
+            }
+            User currUser = _context.users.SingleOrDefault(user => user.userid == currId);
+            ViewBag.user = currUser;
             return View();
+        }
+        [HttpGet]
+        [Route("introduction")]
+        public IActionResult Introduction()
+        {
+            return View();
+        }
+        [HttpGet]
+        [Route("practice")]
+        public IActionResult Practice()
+        {
+            int? currId = HttpContext.Session.GetInt32("currId");
+            if(currId == null)
+            {
+                return RedirectToAction("Logout", "Home");
+            }
+            User currUser = _context.users.SingleOrDefault(user => user.userid == currId);
+            ViewBag.user = currUser;
+            Random rand = new Random();
+            string letters = "abcdefghijklmnopqrstuvwxyz";
+            ViewBag.letter = letters[rand.Next(0,26)] + ".png";
+            return View();
+        }
+        [HttpPost]
+        [Route("practice")]
+        public IActionResult Practice(string practice, string match)
+        {
+            int? currId = HttpContext.Session.GetInt32("currId");
+            if(currId == null)
+            {
+                return RedirectToAction("Logout", "Home");
+            }
+            User currUser = _context.users.SingleOrDefault(user => user.userid == currId);
+            if(practice+".png" == match)
+            {
+                currUser.semxp ++;
+                _context.SaveChanges();
+            }
+            if(currUser.semxp > 99)
+            {
+                currUser.semlevel = 1;
+                _context.SaveChanges();
+            }
+            if(currUser.semxp > 223)
+            {
+                currUser.semlevel = 2;
+                _context.SaveChanges();
+            }
+            if(currUser.semxp > 354)
+            {
+                currUser.semlevel = 3;
+                _context.SaveChanges();
+            }
+            if(currUser.semxp > 475)
+            {
+                currUser.semlevel = 4;
+                _context.SaveChanges();
+            }
+            if(currUser.semxp > 599)
+            {
+                currUser.semlevel = 5;
+                _context.SaveChanges();
+            }
+            if(currUser.semxp > 750)
+            {
+                currUser.semlevel = 6;
+                _context.SaveChanges();
+            }
+            if(currUser.semxp > 900)
+            {
+                currUser.semlevel = 7;
+                _context.SaveChanges();
+            }
+            if(currUser.semxp > 1111)
+            {
+                currUser.semlevel = 8;
+                _context.SaveChanges();
+            }
+            if(currUser.semxp > 1501)
+            {
+                currUser.semlevel = 9;
+                _context.SaveChanges();
+            }
+            if(currUser.semxp > 2123)
+            {
+                currUser.semlevel = 10;
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Practice");
         }
     }
 }
